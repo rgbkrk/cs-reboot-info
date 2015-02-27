05654e0bf82f2c1d08cdeca046ce0052288a428e
jrperritt <jrperritt@gmail.com>
2015-02-27 16:12:43 -0700

    add new line after message

--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func main() {
 		Availability: gophercloud.AvailabilityPublic,
 	})
 	if err != nil {
-		fmt.Printf("Unable to locate a v1 compute endpoint. Skipping...")
+		fmt.Printf("Unable to locate a v1 compute endpoint. Skipping...\n")
 	} else {
 		err = rsV1Servers.List(compute, rsV1Servers.ListOpts{}).EachPage(func(page pagination.Page) (bool, error) {
 			s, err := osV2Servers.ExtractServers(page)
